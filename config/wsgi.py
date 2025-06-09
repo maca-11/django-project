e9a1c715233c582ef2a936be36aa1b147e6996b1
masaki <m.kudou.sys24@morijyobi.ac.jp>
2025-06-09 15:00:41 +0900

    3

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # 追加
