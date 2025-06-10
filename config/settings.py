bafa758d5a809d4980b822973951111e9520a314
masaki <m.kudou.sys24@morijyobi.ac.jp>
2025-06-10 15:42:29 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,8 @@ DATABASES = {
 }
 
 STATIC_URL = "static/"
-STATIC_ROOT = str(BASE_DIR / "static")
+#STATIC_ROOT = str(BASE_DIR / "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 追加
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
